80fa0cbb5064921c968ee597d5f98e8b64f0b301
avikmajumder17 <avikmajumder17@gmail.com>
2026-08-21 00:14:42 +0530

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    // baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://ai-node-backend-kgpo.onrender.com/api/v1",
     headers: {
         "Content-Type": "application/json"
     }
